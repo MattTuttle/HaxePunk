0820eac2def202fb4b6b13bea716fd730c8c1fe4
MattTuttle <heardtheword@gmail.com>
2015-11-11 14:10:39 -0600

    Adding random helper functions and a generic binary insertion sort

--- haxepunk/utils/ArrayUtils.hx
+++ haxepunk/utils/ArrayUtils.hx
@@ -77,6 +77,56 @@ class ArrayUtils
 	}
 
 	/**
+	 * Binary insertion sort
+	 * @param array    The array to insert into
+	 * @param key      The key to insert
+	 * @param compare  A comparison function to determine sort order
+	 */
+	public static function insertSortedKey<T>(array:Array<T>, key:T, compare:T->T->Int):Void
+	{
+		var result:Int = 0,
+			mid:Int = 0,
+			min:Int = 0,
+			max:Int = array.length - 1;
+		while (max >= min)
+		{
+			mid = min + Std.int((max - min) / 2);
+			result = compare(array[mid], key);
+			if (result > 0) max = mid - 1;
+			else if (result < 0) min = mid + 1;
+			else break;
+		}
+
+		array.insert(result > 0 ? mid : mid + 1, key);
+	}
+
+	/**
+	 * Choose a random element from an array.
+	 * @param array  The array to choose from.
+	 * @return The chosen item. If the array is empty a null is returned.
+	 */
+	@:generic
+	public static function choose<T>(array:Array<T>):Null<T>
+	{
+		return array.length == 0 ? null : array[Random.int(array.length)];
+	}
+
+	/**
+	 * Shuffles the elements in the array.
+	 * @param array  The Object to shuffle (an Array or Vector).
+	 */
+	public static function shuffle<T>(array:Array<T>):Void
+	{
+		var i:Int = array.length, j:Int, t:T;
+		while (--i > 0)
+		{
+			t = array[i];
+			array[i] = array[j = Random.int(i + 1)];
+			array[j] = t;
+		}
+	}
+
+	/**
 	 * Empties an array.
 	 */
 	@:generic
