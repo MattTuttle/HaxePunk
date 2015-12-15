a40e29a3eae47303dabbb690e39a07b51814354a
Matt Tuttle <heardtheword@gmail.com>
2015-12-14 22:56:52 -0600

    Small change to example

--- samples/1-helloworld/HelloWorld.hx
+++ samples/1-helloworld/HelloWorld.hx
@@ -1,18 +1,13 @@
 import haxepunk.*;
 import haxepunk.graphics.*;
-import haxepunk.scene.*;
 
 class HelloWorld extends Engine
 {
 	override public function ready(window:Window)
 	{
-		var scene = new Scene();
 		var text = new Text("Hello world", 32);
 		text.color.fromInt(0);
 		text.centerOrigin();
-		scene.addGraphic(text, 0, window.width / 2, window.height / 2);
-
-		window.scene.camera.clearColor = new Color().fromInt(0x333333);
-		window.replaceScene(scene, new Transition(5));
+		window.scene.addGraphic(text, 0, window.width / 2, window.height / 2);
 	}
 }
